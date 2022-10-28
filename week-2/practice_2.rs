d8c21d986c706b4d6879a75f89059f622768817b
Edoziem-first <115808220+Edoziem-first@users.noreply.github.com>
2022-10-28 10:49:46 +0100

    Add files via upload
    I finally understand how to commit my projects. This is my first commit with no help whatsoever!!!!!

---
+++ week-2/practice_2.rs
@@ -0,0 +1,6 @@
+fn main(){
+	println!(); // print just a 
+	println!("hello ");//prints 
+	println!("forest {} arguments", "some"); //prints format some arguments
+	println!("My name is {}. I am the captain of the volleyball team for 100 levels","Nnaedoziem Anike-Nweze");
+}
