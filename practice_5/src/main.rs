42a823ceb7d4ae1cb8f7f9193a35da4cb9d4ae85
Edoziem-first <115808220+Edoziem-first@users.noreply.github.com>
2022-11-11 13:16:22 +0100

    Add files via upload

---
+++ practice_5/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+     let special_character = "@"; //default
+    let alphabet:char = 'A';
+    let surname = "Anike-Nweze";
+     let first_name = "Nnaedoziem"; 
+    let middle_name = "Greene";
+    
+    println!(" ");
+    println!("Special character: {}",special_character );
+    println!("Alphabet: {}",alphabet );
+    println!("Name: {} {} {}",surname, middle_name, first_name );
+}
